abc10066859e0bb2aeb378074e4f9f67c312a21e
Your Name <you@example.com>
2026-03-31 21:11:27 -0400

    fix: corrected requirements.txt formatting for clean Docker build

---
+++ gitter.sh
@@ -0,0 +1,3 @@
+git remote add origin https://github.com/zeshan1977/Maelstrom-Data-Core
+git branch -M main
+git push -u origin main
---
+++ trigger1api.sh
@@ -0,0 +1,1 @@
+curl -X POST http://localhost:8000/api/v1/trigger-etl
